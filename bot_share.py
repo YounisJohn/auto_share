2c5da6055cb810f280aa55c9150cde5e91171517
SACRED GAMES 3.06 <106374961+YounisJohn@users.noreply.github.com>
2022-12-24 10:40:54 +0500

    Update bot_share.py

--- bot_share.py
+++ bot_share.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
    try:
 
-       os.system("git pull");os.system("clear")print('\n\r\033[1;33m Welcome to the Younis XYZ bot boiiw 😝\033[1;37m\n\n');time.sleep(2)
+       os.system("git pull");os.system("clear")print('\n\r\033[1;33m Welcome to the Younis XYZ bot boiiw \033[1;37m\n\n');time.sleep(2)
 
  __import__("YOUNISBOTBOY").Younisxyzbot()
 
